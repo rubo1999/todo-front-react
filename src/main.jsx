b62be1703c49a6c5ecae936dbb87bd3c515c6d5e
Ruben <rubo.lopez.1999@alumnos.cei.es>
2024-06-14 19:06:17 +0200

    Css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Tareas from './Tareas.jsx'
-import './estilo.css'
+import '../public/estilo.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
